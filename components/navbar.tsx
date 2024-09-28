795bc6b7faca75806d11e7a01ab6a917ae50be15
Spencer Cornish <SpencerCornish@users.noreply.github.com>
2024-09-28 00:43:29 -0600

    Cleanup

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -13,9 +13,8 @@ export const Navbar = () => {
     <NextUINavbar maxWidth="xl" position="static">
       <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
         <NavbarBrand as="li" className="gap-3 max-w-fit">
-          <NextLink
+          <div
             className="flex justify-start items-center gap-1"
-            href="/"
           />
         </NavbarBrand>
       </NavbarContent>
